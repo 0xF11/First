01563b60234159b49aec2542c3254fbb4ef80fc8
0xF11 <140972657+0xF11@users.noreply.github.com>
2025-06-04 18:10:27 -0700

    Update ac.cpp

--- ac.cpp
+++ ac.cpp
@@ -1,51 +1,53 @@
 #include "everything.h"
 #include "Proc.h"
 
+
+
+
 int main()
 {
-	DWORD procID = GetProcId(L"ac_client.exe");
-	// Module base Addr
-	uintptr_t moduleBaseAddr = GetModuleBaseAddr(procID, (L"ac_client.exe"));
+    DWORD ProcID = GetProcId(L"ac_client.exe");
+    uintptr_t mbaseaddr = GetModuleBaseAddr(ProcID, L"ac_client.exe");
 
-	// Get handle to proc
-	HANDLE hProcess = 0;
-	hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, procID);
+    HANDLE hP = 0;
+    hP = OpenProcess(PROCESS_ALL_ACCESS, NULL, ProcID);
 
-	// Resolves base address off pointer chain 
+    uintptr_t DMA = mbaseaddr + 0x10f4f4;
+    std::cout << "0x" << std::hex << DMA;
 
-	uintptr_t dynamicbaseaddr = moduleBaseAddr + 0x10F4F4;
+    std::vector<unsigned int>HealthOffset{ 0xf8 };
+    std::vector<unsigned int>AmmoOff{ 0x374, 0x14, 0x0 };
+    std::vector<unsigned int>SniperCoolOff{ 0x174 };
+    uintptr_t healthaddr = FindDMAAddy(hP, DMA, HealthOffset);
+    uintptr_t ammoaddr = FindDMAAddy(hP, DMA, AmmoOff);
+    uintptr_t Cooldownaddr = FindDMAAddy(hP, DMA, SniperCoolOff);
+    std::cout << "Health addr = 0x" << std::hex << healthaddr;
+    std::cout << "\n\n";
 
-	// Resolves relevent ptrs
-
-	std::vector<unsigned int> ammoOffsets = { 0x374, 0x14, 0x0 };
-	std::vector<unsigned int> HealthOffsets = { 0xF8 };
-	uintptr_t CurrAmmo = FindDMAAddy(hProcess, dynamicbaseaddr, ammoOffsets);
-	uintptr_t CurrHealth = FindDMAAddy(hProcess, dynamicbaseaddr, HealthOffsets);
+    int oHP = 0;
+    ReadProcessMemory(hP, (BYTE*)healthaddr, &oHP, sizeof(oHP), 0);
+    std::cout << std::dec << oHP;
 
 
-	// Read
+    int i = 0;
+    
+    do
+    {
 
-	int health = 0;
-	int NewHealth = 50000;
+        int nHP = 9999999;
+        WriteProcessMemory(hP, (BYTE*)healthaddr, &nHP, sizeof(nHP), 0);
+        uintptr_t ammoaddr = FindDMAAddy(hP, DMA, AmmoOff);
+        int oAmmo = 0;
+        ReadProcessMemory(hP, (byte*)ammoaddr, &oAmmo, sizeof(oAmmo), 0);
+        int NAmmo = 777;
+        WriteProcessMemory(hP, (byte*)ammoaddr, &NAmmo, sizeof(NAmmo), 0);
+        int NoCool = 0;
+        WriteProcessMemory(hP, (byte*)Cooldownaddr, &NoCool, sizeof(NoCool), 0);
+    } while (i < 5);
 
-	int ammo = 0;
-	int NewAmmo = 9999;
-	std::cout << "Local Player Addr = " << "0x" << std::hex << dynamicbaseaddr << std::endl;
-	std::cout << "Health addr = " << "0x" << std::hex << CurrHealth << std::endl;
-	std::cout << "Current Weapon Ammo Addr = " << "0x" << std::hex << CurrAmmo << std::endl;
-	std::cout << "Current Ammo = " << std::dec << ammo;
-	std::cout << "Current health = " << std::dec << health;
-	std::cout << "New Current Ammo = " << std::dec << NewAmmo;
-	std::cout << "New Health = " << std::dec << NewHealth;
-	for(int i = 0; i < 9999; i++, i--)
-	{
-		ReadProcessMemory(hProcess, (BYTE*)CurrHealth, &health, sizeof(health), nullptr);
 
-		ReadProcessMemory(hProcess, (BYTE*)CurrAmmo, &ammo, sizeof(ammo), nullptr);
+    // Loop to run shit infinetly
 
-		// Write to ammo value
-		WriteProcessMemory(hProcess, (BYTE*)CurrHealth, &NewHealth, sizeof(NewHealth), nullptr);
-	
-		WriteProcessMemory(hProcess, (BYTE*)CurrAmmo, &NewAmmo, sizeof(NewAmmo), nullptr);
 
-	};
+}
+
